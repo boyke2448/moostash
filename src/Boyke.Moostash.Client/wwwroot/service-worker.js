6577794cfd88203e83c90577c1e4511b60b34c93
boyke2448 <boyke2448@users.noreply.github.com>
2023-01-10 23:48:20 +0100

    Restore auth with server-worker adjustment

--- src/Boyke.Moostash.Client/wwwroot/service-worker.js
+++ src/Boyke.Moostash.Client/wwwroot/service-worker.js
@@ -2,3 +2,12 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
+const navHandler = createHandlerBoundToURL('/index.html');
+const navigationRoute = new NavigationRoute(navHandler, {
+  denylist: [
+    new RegExp('^/images/'),
+    new RegExp('^/static/'),
+    new RegExp('^/.auth/'),
+  ],
+});
+registerRoute(navigationRoute);
